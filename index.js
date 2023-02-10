e265550206833d0ae6a14e2fbd335e63ae8dac57
arafat-khan072 <arafat.khan@digigate360.com>
2023-02-10 22:00:04 +0600

    update cors again

--- index.js
+++ index.js
@@ -6,9 +6,7 @@ const app = express();
 
 const mongoose = require("mongoose");
 
-app.use(cors({
-    origin: "http://localhost:3000"
-}));
+app.use(cors());
 app.use(express.json());
 
 app.use((req, res, next) => {
